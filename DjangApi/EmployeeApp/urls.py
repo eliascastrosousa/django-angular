02a1334336f20048f7df079d49049e1ddf9a51cd
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2023-11-01 10:21:34 -0300

    commit initial

---
+++ DjangApi/EmployeeApp/urls.py
@@ -0,0 +1,15 @@
+from EmployeeApp import views
+from django.urls import re_path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    #path('department/$', views.departmentApi, name='department'),
+    #path('department/<id>', views.departmentApi, name='department'),
+    re_path(r'^department/$', views.departmentApi),
+    re_path(r'^department/([0-9]+)$', views.departmentApi),
+    re_path(r'^employee/$', views.employeeApi),
+    re_path(r'^employee/([0-9]+)$', views.employeeApi),
+    re_path(r'^SaveFile$', views.SaveFile)
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
